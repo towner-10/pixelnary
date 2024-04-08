4fd91223aab2493d6501db3720ee7770e0791f3e
Collin Town <35181633+towner-10@users.noreply.github.com>
2024-04-08 11:36:06 -0400

    Graceful exiting on CTRL-C

--- server/src/Main.cpp
+++ server/src/Main.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <signal.h>
 
 #define USE_LOGGER_MACROS
 #include "Logger.h"
 
 #include "Server.h"
 
+volatile sig_atomic_t stop;
+
+void quitHandle(int signum) {
+    stop = 1;
+}
+
 int main()
 {
     std::cout << "Starting server and logger..." << std::endl;
+
+    signal(SIGINT, quitHandle);
 
     auto& logger = Logger::Get();
     logger.AddOutput(LogLevel::Debug | LogLevel::Info, Logger::StandardOutput);
@@ -17,10 +26,12 @@ int main()
     auto& s = Server::Get();
     s.Start();
 
-    while (true)
+    while (!stop)
     {
         s.HandleMessages();
     }
+
+    s.Stop();
 
     return 0;
 }
